f7fe35474a5d218270e73ecba2fc9bf7d21dd6e2
Jaeri Park <59538964+motonim@users.noreply.github.com>
2021-02-08 21:42:47 -0500

    create card.jsx and make it more clean

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,6 @@
 import React from "react";
+import Card from "./Card";
 import contacts from "../contacts";
-
-function Card(props) {
-  return (
-    <div className="card">
-      <div className="top">
-        <h2 className="name">{props.name}</h2>
-        <img src={props.img} alt="avatar_img" className="circle-img" />
-      </div>
-
-      <div className="bottom">
-        <p className="info">{props.tel}</p>
-        <p className="info">{props.email}</p>
-      </div>
-    </div>
-  );
-}
 
 function App() {
   return (
